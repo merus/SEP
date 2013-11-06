baef5b05314784778aeff95d318c26080b66bcfa
Matt Cramp <mattcrampy@gmail.com>
2013-11-06 15:18:50 +1100

    Started SessionController

---
+++ src/edu/uts/sep/controller/AuthHelper.java
@@ -0,0 +1,9 @@
+package edu.uts.sep.controller;
+
+public class AuthHelper {
+
+	public AuthHelper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/edu/uts/sep/controller/SessionController.java
@@ -0,0 +1,26 @@
+package edu.uts.sep.controller;
+
+import edu.uts.sep.model.PersonDAO;
+
+public class SessionController {
+
+	private PersonDAO personModel;
+	
+	public void setPersonDAO(PersonDAO personModel) {
+		this.personModel = personModel;
+	}
+	
+	public SessionController() {
+		
+	}
+	
+	public void create() {
+		
+	}
+	
+	
+	public void destroy() {
+		session
+	}
+
+}
